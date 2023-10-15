0d2d0342f78c4e1a7276051ba51f4472225c3185
iancostag <146049457+iancostag@users.noreply.github.com>
2023-10-15 12:21:24 -0300

    Add files via upload

---
+++ LINKEDIN-OO-T03-main/LINKEDIN-OO-T03-main/TP03/src/Model/OfertaModel.java
@@ -0,0 +1,60 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class OfertaModel {
+
+    private double salario;
+	private String cargo;
+	private int qntVagas;
+	private LocalDateTime criadoEm;
+	private String escolaridade;
+
+	public OfertaModel(double salario, String cargo, int qntVagas, LocalDateTime criadoEm, String escolaridade) {
+		this.salario = salario;
+		this.cargo = cargo;
+		this.qntVagas = qntVagas;
+		this.criadoEm = criadoEm;
+		this.escolaridade = escolaridade;
+	}
+
+	public String getEscolaridade(){
+		return escolaridade;
+	}
+
+	public void setEscolaridade(String escolaridade){
+		this.escolaridade = escolaridade;
+
+	}
+	public LocalDateTime getCriadoEm(){
+		return criadoEm;
+	}
+
+	public void setCriadoEm(LocalDateTime criadoEm){
+		this.criadoEm = criadoEm;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public int getQntVagas() {
+		return qntVagas;
+	}
+
+	public void setQntVagas(int qntVagas) {
+		this.qntVagas = qntVagas;
+	}
+}
